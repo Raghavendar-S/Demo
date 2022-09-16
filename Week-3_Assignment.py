47435680a6652b396081e27c06c6d3a39b5d0f20
Raghavendar-S <111366291+Raghavendar-S@users.noreply.github.com>
2022-09-16 19:04:14 +0530

    Create Week-3_Assignment.py

---
+++ Week-3_Assignment.py
@@ -0,0 +1,9 @@
+n_row= int(input())
+n_col= int(input())
+array_2d = [[0 for i in range(n_col)] for j in range(n_row)]
+
+for i in range(n_row):
+    for j in range(n_col):
+        multi_list[i][j]= i*j
+
+print(array_2d)
